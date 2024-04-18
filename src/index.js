84fb80e489f42b1984d021d4baa562e1d9449bc9
ChrisM345 <chrism.12277@gmail.com>
2024-04-17 19:46:43 -0500

    Create player and computer gameboard and ships

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
-import { createGameBoard, getGrid } from "./gameboard";
-import { initializeShips, getShips } from "./ship";
+import { createGameboard } from "./gameboard";
+import { setShips } from "./ship";
 
-createGameBoard();
-initializeShips();
-console.log(getShips());
-console.table(getGrid());
+createGameboard();
+// console.log(getShips());
+// console.table(getGrid());
+
+const playerGameboard = createGameboard("player");
+console.log(playerGameboard);
+const computerGameboard = createGameboard("computer");
+const playerShips = setShips(playerGameboard);
+const computerShips = setShips(computerGameboard);
+console.table(playerGameboard.getGrid());
+console.log(playerShips);
+console.table(computerGameboard.getGrid());
+console.log(computerShips);
