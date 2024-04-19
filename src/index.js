a7846fe2b9d03ca9822d56c561f4371e7c9bc00b
ChrisM345 <chrism.12277@gmail.com>
2024-04-18 19:14:22 -0500

    index.js updated to create objects and display gameboard

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import { createGameboard } from "./gameboard";
 import { setShips } from "./ship";
+import { createPlayer } from "./player";
+import { displayGameboards } from "./interface";
+import "./style.css";
 
 createGameboard();
 // console.log(getShips());
@@ -14,3 +17,10 @@ console.table(playerGameboard.getGrid());
 console.log(playerShips);
 console.table(computerGameboard.getGrid());
 console.log(computerShips);
+
+const player = createPlayer("player", playerGameboard, playerShips);
+const computer = createPlayer("computer", computerGameboard, computerShips);
+console.log(player);
+console.log(computer);
+displayGameboards(player);
+displayGameboards(computer);
