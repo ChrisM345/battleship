9d1fb9a0761ea2782e7d4fe3e3562bc0a84345dd
ChrisM345 <chrism.12277@gmail.com>
2024-04-19 17:24:09 -0500

    Added total ships to class

--- src/player.js
+++ src/player.js
@@ -1,13 +1,14 @@
 class Player {
-  constructor(name, gameboard, ships) {
+  constructor(name, gameboard, ships, totalShips) {
     this.name = name;
     this.gameboard = gameboard;
     this.ships = ships;
+    this.totalShips = totalShips;
   }
 }
 
 function createPlayer(name, gameboard, ships) {
-  return new Player(name, gameboard, ships);
+  return new Player(name, gameboard, ships, 5);
 }
 
 export { createPlayer };
